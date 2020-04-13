bf05351afd30626535a5a28c1fd276329d1a86ea
hobbyluv <princewillchime43@gmail.com>
2020-04-13 03:02:46 +0100

    adjusted the footer css and components

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -3,25 +3,43 @@ import FaceBookLogo from "./Images/twitter-black-shape.svg";
 import LinkedinLogo from "./Images/linked-in-logo-of-two-letters.svg";
 import Email from "./Images/envelope.svg";
 import UnichrisLogo from "../unichris logo.png";
-import "./Footer.css";
-const TopArea = () => {
+import FooterStyle from "./Footer.module.css";
+import CustomImage from "../Reuseable.component/Image.component/Image";
+import CustomLink from "../Reuseable.component/Link.component/Link";
+const TopArea = ({
+  social_media_contact_wrapper,
+  school_hint,
+  school_phone_contact,
+  social_media_logos,
+  motto
+}) => {
   return (
-    <div className="img">
-      <div className="case">
-        <img src={UnichrisLogo} alt={UnichrisLogo} />
-        <div>
+    <div className={social_media_contact_wrapper}>
+      <div className={school_hint}>
+        <CustomImage src={UnichrisLogo} alt={UnichrisLogo} />
+        <div className={motto}>
           <h4>unichris library</h4>
           <p>my home! our home!!</p>
         </div>
       </div>
       <br />
-      <Contacts />
+      <Contacts
+        school_phone_contact={school_phone_contact}
+        social_media_logos={social_media_logos}
+      />
     </div>
   );
 };
-const Contacts = () => {
+const Contacts = ({ school_phone_contact, social_media_logos }) => {
+  const phoneArray = [
+    "090-377-57032",
+    "090-377-57033",
+    "090-377-57036",
+    "090-377-57037"
+  ];
+  const logoArray = [FaceBookLogo, LinkedinLogo, Email];
   return (
-    <div className="f-tex">
+    <div>
       <p>
         want to go on research of all sort,
         <br />
@@ -30,73 +48,95 @@ const Contacts = () => {
         you wish to acces and more
       </p>
       <br />
-      <div className="f-phon">
+      <div className={school_phone_contact}>
         <h3>phone number</h3>
-        <i>090-377-57032</i>
-        <i>090-377-57033</i>
-        <i>090-377-57036</i>
-        <i>090-377-57037</i>
-      </div>
-      <div className="con-link">
         <br />
-        <a href="/">
-          <img src={FaceBookLogo} alt={FaceBookLogo} width="10%" />
-        </a>
-        <a href="/">
-          <img src={LinkedinLogo} alt={LinkedinLogo} />
-        </a>
-        <a href="/">
-          <img src={Email} alt={Email} />
-        </a>
+        {phoneArray.map((numbers, i) => (
+          <i key={i}>{numbers}</i>
+        ))}
+      </div>
+      <div className={social_media_logos}>
+        <br />
+        {logoArray.map((socialHandle_logos, i) => (
+          <CustomLink
+            key={i}
+            url={"/"}
+            text={
+              <CustomImage
+                src={socialHandle_logos}
+                alt={"social media handle logo"}
+              />
+            }
+          />
+        ))}
       </div>
     </div>
   );
 };
 
-const Services = () => {
+const Services = ({ service_wrapper, service_links_wrapper }) => {
+  const serviceListArray = [
+    "About unchris",
+    "Admmision",
+    "Employment",
+    "Courses",
+    "Jupeb program",
+    "Icbt centre"
+  ];
   return (
-    <div className="f-link">
+    <div className={service_wrapper}>
       <h3>services</h3>
       <br />
-      <div className="f-a">
-        <a href="/">About unchris</a>
-        <a href="/">Admmision</a>
-        <a href="/">Employment</a>
-        <a href="/">Courses</a>
-        <a href="/">Jupeb program</a>
-        <a href="/">Icbt centre</a>
+      <div className={service_links_wrapper}>
+        {serviceListArray.map((schoolOffers, i) => (
+          <CustomLink url={"/"} text={schoolOffers} key={i} />
+        ))}
       </div>
     </div>
   );
 };
 
-const Categories = () => {
+const Categories = ({ footer_categories, categories_list }) => {
+  const categories_listArray = [
+    "fiction books",
+    "non-fiction",
+    "newspaper",
+    "journal",
+    "encyclopedias",
+    "history"
+  ];
   return (
-    <div className="f-cat">
+    <div className={footer_categories}>
       <h3>categories</h3>
       <br />
-      <div className="cat-lis">
-        <a href="/">fiction books</a>
-        <a href="/">non-fiction</a>
-        <a href="/">newspaper</a>
-        <a href="/">journal</a>
-        <a href="/">encyclopedias</a>
-        <a href="/">history</a>
+      <div className={categories_list}>
+        {categories_listArray.map((libraryHoldings, i) => (
+          <CustomLink url={"/"} text={libraryHoldings} key={i} />
+        ))}
       </div>
     </div>
   );
 };
-const EasyLink = () => {
+const EasyLink = ({
+  footer_easy_links,
+  categories_list,
+  school_email_link
+}) => {
+  const easyLinkArray = [
+    "books on literature",
+    "books on social sciences",
+    "books on mass communication"
+  ];
   return (
-    <div className="f-easy">
+    <div className={footer_easy_links}>
       <h3>easy links</h3>
       <br />
-      <div className="cat-lis">
-        <a href="/">books on literature</a>
-        <a href="/">books on social sciences</a>
-        <a href="/">books on mass communication</a>
+      <div className={categories_list}>
+        {easyLinkArray.map((eBooks, i) => (
+          <CustomLink url={"/"} text={eBooks} key={i} />
+        ))}
       </div>
-      <div className="emai">
+      <div className={school_email_link}>
         <a href="mailto:princewillchime43@gmail.com? subject=subject text">
           email
         </a>
@@ -106,16 +146,49 @@ const EasyLink = () => {
 };
 class Footer extends Component {
   render() {
+    const {
+      footer_wrapper,
+      social_media_contact_wrapper,
+      school_hint,
+      school_phone_contact,
+      social_media_logos,
+      service_wrapper,
+      service_links_wrapper,
+      footer_categories,
+      categories_list,
+      footer_easy_links,
+      school_email_link,
+      copy_write,
+      motto
+    } = FooterStyle;
     return (
       <footer>
-        <div className="ff">
-          <TopArea />
-          <Services />
-          <Categories />
-          <EasyLink />
-        </div>
-        <div className="ryt">
-          <h4>copyright 2019 christopher university</h4>
+        <div>
+          <div className={footer_wrapper}>
+            <TopArea
+              social_media_contact_wrapper={social_media_contact_wrapper}
+              school_hint={school_hint}
+              school_phone_contact={school_phone_contact}
+              social_media_logos={social_media_logos}
+              motto={motto}
+            />
+            <Services
+              service_wrapper={service_wrapper}
+              service_links_wrapper={service_links_wrapper}
+            />
+            <Categories
+              footer_categories={footer_categories}
+              categories_list={categories_list}
+            />
+            <EasyLink
+              footer_easy_links={footer_easy_links}
+              school_email_link={school_email_link}
+              categories_list={categories_list}
+            />
+          </div>
+          <div className={copy_write}>
+            <h4>copyright 2019 christopher university</h4>
+          </div>
         </div>
       </footer>
     );
